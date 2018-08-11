8a0db2a998043da7a446f5c8034e00e5f2f76d10
Micheal Uchenna Nnani <uchenna.nnani@outlook.com>
2018-08-11 01:51:22 +0100

    An api for my facerecognition project

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "a090d3b1672042f3988eecf6bce694dd"
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage:handleImage,
+    handleApiCall:handleApiCall
+}
